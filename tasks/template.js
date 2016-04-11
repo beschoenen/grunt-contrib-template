ad880586dc6c49d646db66fac48caccab090ebf7
Kevin Richter <me@kevinrichter.nl>
2016-04-11 14:41:22 +0200

    WIP sections

--- tasks/template.js
+++ tasks/template.js
@@ -22,28 +22,47 @@ module.exports = function(grunt) {
       grunt.fail.warn(chalk.red("dest is required."));
     }
 
+    var fileRegex = /(?:["'])<!=\s*(.+)\b\s*!>(?:["'])?;/;
+    var fileExtRegex = /\..+$/;
+    var sectionRegex = /#(.+)$/;
+
     // Recursively resolve the template
     function getSourceCode(filepath, section) {
-      var fileRegex = /(?:["'])<!=\s*(.+)\b\s*!>(?:["'])?;/;
-      var fileExtRegex = /\..+$/;
-
       return grunt.file.read(filepath).replace(new RegExp(fileRegex.source, "g"), function(match) {
-        files++; // Log the number of imports we did
+        // Log the number of imports we did
+        files++;
 
         // Get the filename
         var file = match.match(fileRegex)[1];
-        // Check if it has an extension
+
+        // Find the section
+        var section = file.match(sectionRegex) || null;
+
+        // Remove the section from the file name
+        if(section !== null) {
+          file = file.replace(sectionRegex, '');
+        }
+
+        // Check if the filename has an extension
         if(file.match(fileExtRegex) === null) {
           file += options.defaultExtension; // Add it
         }
 
         // Parse the file path
         var fileObject = parsePath(filepath);
-        // trim slashes from the file path
+
+        // Trim slashes from the file path
         var newFile = (fileObject.dirname + "/" + file).replace(/^\/|\/$/g, '');
 
-        // Read file
+        // Read the file
         var src = grunt.file.read(newFile);
+
+        // Get the section
+        if(section !== null) {
+          src = src.match(makeSectionRegex(/\/\/\s?#!\s?bar((?:.|\n)*)\/\/\s?#\s?stop/gm));
+        }
+
+        console.log(src);
 
         // Check if it has imports too
         return (function() {
@@ -54,6 +73,10 @@ module.exports = function(grunt) {
               }
             })() + options.separator; // Add separator
       });
+    }
+
+    function makeSectionRegex(section) {
+      return new RegExp("\/\/\/s?#!\/s?" + section + "(\/s|.)*\/\/\/s?#\/s?stop", "gm");
     }
 
     // Make the template
